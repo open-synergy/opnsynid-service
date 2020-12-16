72d8df87591237a03cea96dec2c8e64d2d534eeb
Andhitia Rama <andhitia.r@gmail.com>
2020-12-17 01:05:33 +0700

    [service_contract_quotation_operating_unit] 8.0.1.0.0

---
+++ service_contract_quotation_operating_unit/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    service_quotation,
+    service_contract_type_operating_unit,
+)
