5a6ab5573df15ad4037b081c2817942932c81720
mikevhe18 <viriyananda.michael@gmail.com>
2021-02-10 11:19:40 +0700

    [service_contract_quotation_project] 8.0.1.0.0

---
+++ service_contract_quotation_project/__openerp__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Service Contract Quotation Project",
+    "version": "8.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "license": "AGPL-3",
+    "depends": [
+        "service_contract_project",
+        "service_contract_quotation",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
