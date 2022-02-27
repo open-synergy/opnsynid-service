3c5219586270b870bf899d0a04c8071bcb47010d
Andhitia Rama <andhitia.r@gmail.com>
2022-02-27 19:20:21 +0700

    [service_contract] 8.0.4.7.1
    * Update AA when already exist

--- service_contract/__openerp__.py
+++ service_contract/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Service Contract",
-    "version": "8.0.4.7.0",
+    "version": "8.0.4.7.1",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "website": "https://simetri-sinergi.id",
     "license": "AGPL-3",
