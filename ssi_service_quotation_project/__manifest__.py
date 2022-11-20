22a5c10c7b7dae402171ee62b6540441fcaa7859
Andhitia Rama <andhitia.r@gmail.com>
2022-11-21 02:19:35 +0700

    [ssi_service_quotation_project] 14.0.1.0.0

---
+++ ssi_service_quotation_project/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Service Quotation - Project Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_service_quotation",
+        "ssi_service_project",
+    ],
+    "data": [],
+    "demo": [],
+}
