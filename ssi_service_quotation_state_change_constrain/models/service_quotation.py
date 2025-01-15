8e04deb5eafeeef38ae8b128bd3a6f801526ce21
Andhitia Rama <andhitia.r@gmail.com>
2025-01-15 14:39:36 +0700

    [UPD] ssi_service_quotation_state_change_constrain

--- ssi_service_quotation_state_change_constrain/models/service_quotation.py
+++ ssi_service_quotation_state_change_constrain/models/service_quotation.py
@@ -20,3 +20,10 @@ class ServiceQuotation(models.Model):
         self.status_check_template_id = False
         if self.type_id:
             self.status_check_template_id = self._get_template_status_check()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        _super = super(ServiceQuotation, self)
+        quotation = _super.create(vals_list)
+        quotation.onchange_status_check_template_id()
+        return quotation
