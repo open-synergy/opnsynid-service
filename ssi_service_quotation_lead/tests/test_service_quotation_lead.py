9dd4d8c4a2195907efd5153d869983a8cd27bc91
Andhitia Rama <andhitia.r@gmail.com>
2026-05-17 00:09:06 +0700

    [UPD] ssi_service_quotation_lead
    * Add unit tests

---
+++ ssi_service_quotation_lead/tests/test_service_quotation_lead.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestSvcQuotLead(YamlTransactionCase):
+    def test_service_quotation_lead(self):
+        self.run_yaml_scenario("test_data_service_quotation_lead.yaml")
